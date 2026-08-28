e5b8a0b135479d36de166ce29a3f47763693f4dc
Artur Angiel <arturangiel@gmail.com>
2026-08-28 17:17:09 +0200

    adds disable-mouse function; makes right cmd => super, for s-' (next-window-any-frame)

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
   (setq ns-control-modifier 'control)
   (setq ns-option-modifier 'meta)
   (setq ns-right-alternate-modifier 'none)
-  (setq ns-right-command-modifier 'meta)
+  (setq ns-right-command-modifier 'super)
   (setq ns-right-control-modifier 'control)
   (setq ns-right-option-modifier 'none))
 
@@ -119,4 +119,12 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-c C-f") 'helm-find-files)
 
+(defun disable-mouse ()
+  (interactive)
+  (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]  
+               [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+               [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+               [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+               [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+    (global-unset-key k)))
 
